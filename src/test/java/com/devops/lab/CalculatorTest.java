a24e274ab9f0a550c89b794d61c7db1020a0cede
BalarajMane <rajmane7676@gmail.com>
2026-03-12 10:09:14 +0530

    Update CalculatorTest.java

--- src/test/java/com/devops/lab/CalculatorTest.java
+++ src/test/java/com/devops/lab/CalculatorTest.java
@@ -9,11 +9,15 @@ public class CalculatorTest {
 
     @Test
     void testAdd() {
-        assertEquals(10, calculator.add(5,5));
+        assertEquals(10, calculator.add(5, 5));
+        assertEquals(0, calculator.add(0, 0));
+        assertEquals(-5, calculator.add(-2, -3));
     }
 
     @Test
     void testMultiply() {
-        assertEquals(25, calculator.multiply(5,5));
+        assertEquals(25, calculator.multiply(5, 5));
+        assertEquals(0, calculator.multiply(5, 0));
+        assertEquals(-6, calculator.multiply(-2, 3));
     }
 }
